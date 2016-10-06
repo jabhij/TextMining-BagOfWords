1ba215b80f080547158bf38263e3850d01a621f4
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-10-06 13:51:20 +0530

    Cleaning with qdap

---
+++ TextMining_BagOfWords_Approach/Cleaning_Preprocessing_Text/2-CleaningWith_Qdap.r
@@ -0,0 +1,27 @@
+"""
+Instructions--
+pply the following functions to the text object from the previous exercise:
+
+bracketX()
+replace_number()
+replace_abbreviation()
+replace_contraction()
+replace_symbol()
+"""
+
+## text is still loaded in your workspace
+
+# Remove text within brackets
+bracketX(text)
+
+# Replace numbers with words
+replace_number(text)
+
+# Replace abbreviations
+replace_abbreviation(text)
+
+# Replace contractions
+replace_contraction(text)
+
+# Replace symbols with words
+replace_symbol(text)
