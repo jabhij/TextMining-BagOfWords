e39426ea863335c2943c7f2d1aab6550ae37efd2
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-10-06 13:43:46 +0530

    Common Cleaning Functions from tm

---
+++ TextMining_BagOfWords_Approach/Cleaning_Preprocessing_Text/CleaningFunctions.r
@@ -0,0 +1,27 @@
+"""
+Instructions --
+
+- Create an object, text, containing the sentence below:
+<b>She</b> woke up at       6 A.M. It\'s so early!  She was only 10% awake and began drinking coffee in front of her computer.
+
+- Apply each of the following functions to text, simply printing results to the console:
+tolower()
+removePunctuation()
+removeNumbers()
+stripWhitespace()
+"""
+
+# Create the object: text
+text <- '<b>She</b> woke up at       6 A.M. It\'s so early!  She was only 10% awake and began drinking coffee in front of her computer.'
+
+# All lowercase
+tolower(text)
+
+# Remove punctuation
+removePunctuation(text)
+
+# Remove numbers
+removeNumbers(text)
+
+# Remove whitespace
+stripWhitespace(text)
