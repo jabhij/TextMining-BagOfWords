f06db1b84f2b861033f259897383d13f85a6c79b
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-10-07 12:52:42 +0530

    A Simple Word Cloud

---
+++ WordClouds_InteractingVisuals/Intro_WordCloud/1-Simple_WordCloud.r
@@ -0,0 +1,13 @@
+"""
+Instructions --
+
+- Load the wordcloud package.
+- Print out first 10 entries in term_frequency.
+- Create word_freqs, a data frame with two columns:
+ term = names(term_frequency)
+ num = term_frequency
+
+- Create a wordcloud() using word_freqs$term as the words, and word_freqs$num as the frequencies. Add the parameters max.words = 100 and
+colors = "red".
+"""
+
